be04c7f16f4eaf2edb232f1d7f33080e79c1e8f7
luomeng <mluo@ilegendSoft.com>
2016-03-24 12:10:28 +0800

    show all supported font

---
+++ Categories/Categories/ViewControllers/FontSampleViewController.h
@@ -0,0 +1,13 @@
+//
+//  FontSampleViewController.h
+//  Categories
+//
+//  Created by luomeng on 16/3/24.
+//  Copyright © 2016年 luomeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FontSampleViewController : UITableViewController
+
+@end
---
+++ Categories/Categories/ViewControllers/FontSampleViewController.m
@@ -0,0 +1,99 @@
+//
+//  FontSampleViewController.m
+//  Categories
+//
+//  Created by luomeng on 16/3/24.
+//  Copyright © 2016年 luomeng. All rights reserved.
+//
+
+#import "FontSampleViewController.h"
+
+@interface FontSampleViewController ()
+@property (nonatomic, strong) NSMutableArray *fontNames;
+@end
+
+@implementation FontSampleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    self.fontNames = [[NSMutableArray alloc] init];
+    for (NSString *family in [UIFont familyNames]) {
+        for (NSString *fontName in [UIFont fontNamesForFamilyName:family]) {
+            [self.fontNames addObject:fontName];
+            
+        }
+    }
+    
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.fontNames.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    
+    // Configure the cell...
+    NSString *fontName = self.fontNames[indexPath.row];
+    cell.textLabel.text = fontName;
+    cell.textLabel.font = [UIFont fontWithName:fontName size:16];
+    
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
